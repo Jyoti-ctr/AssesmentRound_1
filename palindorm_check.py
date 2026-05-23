89bafa2a5104193fb2ca7ec39013d4e6d6e2bee4
Your Name <you@example.com>
2026-05-23 12:41:55 +0530

    Assesment submission try

--- palindorm_check.py
+++ palindorm_check.py
@@ -2,10 +2,10 @@ def is_palindrome(s):
     # Remove non-alphanumeric characters and convert to lowercase
     cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
     
-    # Check if the cleaned string is equal to its reverse
+    # Checking if the cleaned string is equal to its reverse
     return cleaned_s == cleaned_s[::-1]
 
-# Get user input
+# user input
 input_string = input("Enter a string to check if it's a palindrome: ")
 # Check if the input string is a palindrome and print the result
 if is_palindrome(input_string):
@@ -13,3 +13,10 @@ if is_palindrome(input_string):
 else:
 
     print(f'"{input_string}" is not a palindrome.')
+
+counter = 0
+for i in range(0, len(input_string)-1+1, 1):
+    for j in range(i+1, len(input_string)-1+1, 1):
+        if input_string[i] == input_string[j]:
+            counter += 1
+print(f'Number of duplicate characters in the input string: {counter}')
--- word_frenquency.py
+++ word_frenquency.py
@@ -2,11 +2,9 @@ stringInput=input("Enter the Sentence: ") # user Input
 Words=[]
 Words.append(stringInput)
 count=0
+
 for i in range(0, len(Words)-1+1, 1):
     for j in range(i+1,len(Words)-1+1, 1) :
         if Words[i]==Words[j]:
             count=count+1
-            
-print(count)
-print(stringInput)
-print()
+print("The frequency of the word is: ",count)
